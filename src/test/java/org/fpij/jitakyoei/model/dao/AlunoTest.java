c866dee6a2e8a4d53151d9d271292c06b8807595
schanz <57364626+gbschanz@users.noreply.github.com>
2024-12-01 18:59:48 -0300

    Update AlunoTest.java

--- src/test/java/org/fpij/jitakyoei/model/dao/AlunoTest.java
+++ src/test/java/org/fpij/jitakyoei/model/dao/AlunoTest.java
@@ -1,20 +1,9 @@
-package org.fpij.jitakyoei.model.dao;
-
 import static org.junit.Assert.*;
-
-import java.util.List;
+import org.fpij.jitakyoei.model.beans.*;
+import org.fpij.jitakyoei.util.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import org.fpij.jitakyoei.model.beans.Aluno;
-import org.fpij.jitakyoei.model.beans.Endereco;
-import org.fpij.jitakyoei.model.beans.Entidade;
-import org.fpij.jitakyoei.model.beans.Filiado;
-import org.fpij.jitakyoei.model.beans.Professor;
-import org.fpij.jitakyoei.model.dao.DAO;
-import org.fpij.jitakyoei.model.dao.DAOImpl;
-import org.fpij.jitakyoei.util.DatabaseManager;
 
 public class AlunoTest {
     private static Aluno aluno;
