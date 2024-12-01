846f5495ca2d35e6d1ae99f505b341be76b8798d
schanz <57364626+gbschanz@users.noreply.github.com>
2024-12-01 16:46:56 -0300

    Delete src directory

--- src/main/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
+++
@@ -1,9 +0,0 @@
-package org.fpij.jitakyoei.model.validator;
-
-import org.fpij.jitakyoei.model.beans.Aluno;
-// teste de ci
-public class AlunoValidator implements Validator<Aluno>{
-	public boolean validate(Aluno obj) {
-		return true;
-	}
-}
