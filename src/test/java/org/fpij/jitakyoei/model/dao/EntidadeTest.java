becf6ae2096674a5b93e484be0e15ee00343c7eb
schanz <57364626+gbschanz@users.noreply.github.com>
2024-12-01 18:59:25 -0300

    Update EntidadeTest.java

--- src/test/java/org/fpij/jitakyoei/model/dao/EntidadeTest.java
+++ src/test/java/org/fpij/jitakyoei/model/dao/EntidadeTest.java
@@ -1,20 +1,13 @@
-package org.fpij.jitakyoei.model.dao;
-
 import static org.junit.Assert.*;
+import org.fpij.jitakyoei.model.beans.*;
+import org.fpij.jitakyoei.util.*;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import org.fpij.jitakyoei.model.beans.Aluno;
-import org.fpij.jitakyoei.model.beans.Endereco;
-import org.fpij.jitakyoei.model.beans.Entidade;
-import org.fpij.jitakyoei.model.beans.Filiado;
-import org.fpij.jitakyoei.model.beans.Professor;
-import org.fpij.jitakyoei.model.dao.DAO;
-import org.fpij.jitakyoei.model.dao.DAOImpl;
-import org.fpij.jitakyoei.util.DatabaseManager;
 
 public class EntidadeTest {
     private static Entidade entidade;
