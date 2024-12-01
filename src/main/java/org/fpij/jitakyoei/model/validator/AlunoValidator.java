ef53a9ddd805530a0adb144c3092f52e492e687c
Leocandido <64386249+Leocandido@users.noreply.github.com>
2024-12-01 15:21:36 -0300

    Update AlunoValidator.java

--- src/main/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
+++ src/main/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
@@ -1,7 +1,7 @@
 package org.fpij.jitakyoei.model.validator;
 
 import org.fpij.jitakyoei.model.beans.Aluno;
-
+// teste de ci
 public class AlunoValidator implements Validator<Aluno>{
 	public boolean validate(Aluno obj) {
 		return true;
