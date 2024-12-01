07a7e66bf17b860b463e1ec3109e4fe16f4dbe83
schanz <57364626+gbschanz@users.noreply.github.com>
2024-12-01 16:56:41 -0300

    Add files via upload

---
+++ src/main/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
@@ -0,0 +1,9 @@
+package org.fpij.jitakyoei.model.validator;
+
+import org.fpij.jitakyoei.model.beans.Aluno;
+// teste de ci
+public class AlunoValidator implements Validator<Aluno>{
+	public boolean validate(Aluno obj) {
+		return true;
+	}
+}
