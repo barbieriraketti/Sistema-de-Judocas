5e5dd1946abd838101feb052cd84f7b73a4c0d61
schanz <57364626+gbschanz@users.noreply.github.com>
2024-12-01 18:49:50 -0300

    Update ProfessorTest.java

--- src/test/java/org/fpij/jitakyoei/model/dao/ProfessorTest.java
+++ src/test/java/org/fpij/jitakyoei/model/dao/ProfessorTest.java
@@ -1,8 +1,6 @@
 package org.fpij.jitakyoei.model.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Date;
