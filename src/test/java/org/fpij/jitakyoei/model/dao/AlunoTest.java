6b00839f6c17129a85ea914487b17318cfac5d63
schanz <57364626+gbschanz@users.noreply.github.com>
2024-12-01 18:30:13 -0300

    Update AlunoTest.java

--- src/test/java/org/fpij/jitakyoei/model/dao/AlunoTest.java
+++ src/test/java/org/fpij/jitakyoei/model/dao/AlunoTest.java
@@ -21,7 +21,7 @@ import org.fpij.jitakyoei.model.dao.DAO;
 import org.fpij.jitakyoei.model.dao.DAOImpl;
 import org.fpij.jitakyoei.util.DatabaseManager;
 
-public class AlunoDaoTest {
+public class AlunoTest {
     private static Aluno aluno;
 
     @BeforeClass
